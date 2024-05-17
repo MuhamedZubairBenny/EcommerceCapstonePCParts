eae66e686f0e5d2455309a0ad46c3479f878448a
Isa-Hassan <isahasssan@gmail.com>
2024-05-17 16:47:05 +0200

    Initial commit

--- src/main/java/za/ac/cput/domain/Order.java
+++ src/main/java/za/ac/cput/domain/Order.java
@@ -1,4 +1,116 @@
 package za.ac.cput.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+import java.util.Objects;
+
+
 public class Order {
+
+    private String orderId;
+    private double overallPrice;
+    private Delivery delivery;
+    private Customer customer;
+    private OrderItem orderItem;
+
+    protected Order(){}
+
+    public Order(Builder builder){
+    this.orderId = builder.orderId;
+    this.overallPrice = builder.overallPrice;
+    this.delivery = builder.delivery;
+    this.customer = builder.customer;
+    this.orderItem = builder.orderItem;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public double getOverallPrice() {
+        return overallPrice;
+    }
+
+    public Delivery getDelivery() {
+        return delivery;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public OrderItem getOrderItem() {
+        return orderItem;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Double.compare(overallPrice, order.overallPrice) == 0 && Objects.equals(orderId, order.orderId) && Objects.equals(delivery, order.delivery) && Objects.equals(customer, order.customer) && Objects.equals(orderItem, order.orderItem);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId, overallPrice, delivery, customer, orderItem);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", overallPrice=" + overallPrice +
+                ", delivery=" + delivery +
+                ", customer=" + customer +
+                ", orderItem=" + orderItem +
+                '}';
+    }
+
+    public static class Builder{
+        private String orderId;
+        private double overallPrice;
+        private Delivery delivery;
+        private Customer customer;
+        private OrderItem orderItem;
+
+        public Builder setOrderId(String orderId) {
+            this.orderId = orderId;
+            return this;
+        }
+
+        public Builder setOverallPrice(double overallPrice) {
+            this.overallPrice = overallPrice;
+            return this;
+        }
+
+        public Builder setDelivery(Delivery delivery) {
+            this.delivery = delivery;
+            return this;
+        }
+
+        public Builder setCustomer(Customer customer) {
+            this.customer = customer;
+            return this;
+        }
+
+        public Builder setOrderItem(OrderItem orderItem) {
+            this.orderItem = orderItem;
+            return this;
+        }
+        public Builder copy(Order order){
+            this.orderId = order.orderId;
+            this.orderItem = order.orderItem;
+            this.overallPrice = order.overallPrice;
+            this.customer = order.customer;
+            this.delivery = order.delivery;
+            return this;
+        }
+        public Order build(){
+            return new Order(this);
+        }
+    }
 }
--- src/main/java/za/ac/cput/domain/OrderItem.java
+++ src/main/java/za/ac/cput/domain/OrderItem.java
@@ -1,4 +1,112 @@
 package za.ac.cput.domain;
 
-public class OrderItem {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+
+public class OrderItem{
+
+    private String itemId;
+    private String item;
+    private Double price;
+    private String description;
+    private int quantity;
+    protected OrderItem(){}
+
+    public OrderItem(Builder builder){
+        this.itemId = builder.itemId;
+        this.item = builder.item;
+        this.description=builder.description;
+        this.price= builder.price;
+        this.quantity = builder.quantity;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return quantity == orderItem.quantity && Objects.equals(itemId, orderItem.itemId) && Objects.equals(item, orderItem.item) && Objects.equals(price, orderItem.price) && Objects.equals(description, orderItem.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(itemId, item, price, description, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "itemId='" + itemId + '\'' +
+                ", item='" + item + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+    public static class Builder{
+        private String itemId;
+        private String item;
+        private Double price;
+        private String description;
+        private int quantity;
+
+        public Builder setItemId(String itemId) {
+            this.itemId = itemId;
+            return this;
+        }
+
+        public Builder setItem(String item) {
+            this.item = item;
+            return this;
+        }
+
+        public Builder setPrice(Double price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setQuantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+        public Builder copy(OrderItem orderItem){
+        this.itemId = orderItem.itemId;
+        this.item = orderItem.item;
+        this.description = orderItem.description;
+        this.price = orderItem.price;
+        this.quantity = orderItem.quantity;
+        return this;
+        }
+        public OrderItem build(){
+            return new OrderItem(this);
+        }
+    }
 }
