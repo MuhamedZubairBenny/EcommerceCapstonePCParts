1d50cabd5eca887c1664497d2a5e0574d7c1a208
Muhamed Zubair Benny <220527636@mycput.ac.za>
2024-08-05 19:40:44 +0200

    NavBar

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,7 +1,6 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'; // Import the correct functions
 import HomePage from '@/components/HomePage.vue';
-import HelloWorld from '@/components/HelloWorld.vue';
 import IntelCoreI9 from '@/components/IntelCoreI9.vue'
 import CustomerTest from "@/components/CustomerTest.vue";
 
@@ -16,15 +15,11 @@ const router = createRouter({
             component: HomePage
         },
         {
-            path: '/hello',
-            name: 'HelloWorld',
-            component: HelloWorld
-        },
-        {
             path: '/CPU',
             name: 'IntelCoreI9',
             component: IntelCoreI9
-=======
+        },
+        {
             path: '/Customer',
             name: 'CustomerTest',
             component: CustomerTest
