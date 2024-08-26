fa49d5342e4e5a5a2cae640a12e0efe04094af14
Isa-Hassan <222043555@mycput.ac.za>
2024-08-26 16:00:53 +0200

    Updated Project 26/08/2024 -Isa Hassan

---
+++ src/frontend/src/components/nvidia/series3000/3060GPUs/3060TiAorusElite.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>3060 Ti Aorus Elite</h1>
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RTX3060'
+}
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
---
+++ src/frontend/src/components/nvidia/series3000/3060GPUs/Dual3060TiMini.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Dual 3060 Ti Mini</h1>
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RTX3060'
+}
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
